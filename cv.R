4228c2f20bdbe8855ea1f2e27a9275ad2d5bc037
Dennis Wollersheim <dewoller@gmail.com>
2020-09-07 18:30:12 +1000

    final ; ;

---
+++ cv.R
@@ -0,0 +1,28 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Dennis Wollersheim",
+  data_location = "https://docs.google.com/spreadsheets/d/1HPZjdhRq8xLfsWQI2AJXZ_ytuWiEktI93Rg_qz_85a4/edit?usp=sharing",
+  pdf_location = "https://github.com/dewoller/cv/raw/master/dewoller_cv.pdf",
+  html_location = "",
+  source_location = "https://github.com/dewoller/cv",
+  open_files=FALSE
+)
+
+devtools::install_github("nstrayer/datadrivencv")
+
+params = list(pdf_mode = FALSE)
+
+
+# Knit the HTML version
+rmarkdown::render("cv.Rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "docs/cv.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("cv.Rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "docs/cv.pdf")
---
+++ cv.md
@@ -0,0 +1,50 @@
+---
+title: "Dennis Wollersheim's CV"
+author: Dennis Wollersheim
+date: "2020-05-23"
+params:
+  pdf_mode:
+    value: true
+output:
+  pagedown::html_resume:
+    css: ['dd_cv.css', 'resume']
+    self_contained: true
+---
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
