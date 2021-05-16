9ff499979976d926f9956539affefeb63e88f309
Dennis Wollersheim <dewoller@gmail.com>
2021-05-17 05:45:53 +1000

    publish

--- cv.R
+++ cv.R
@@ -1,8 +1,8 @@
 datadrivencv::use_datadriven_cv(
   full_name = "Dennis Wollersheim",
   data_location = "https://docs.google.com/spreadsheets/d/1HPZjdhRq8xLfsWQI2AJXZ_ytuWiEktI93Rg_qz_85a4/edit?usp=sharing",
-  pdf_location = "https://github.com/dewoller/cv/raw/master/dewoller_cv.pdf",
-  html_location = "",
+  pdf_location = "https://github.com/dewoller/cv/raw/master/docs/cv.pdf",
+  html_location = "https://dewoller.github.io/cv/cv.html",
   source_location = "https://github.com/dewoller/cv",
   open_files=FALSE
 )
@@ -26,3 +26,4 @@ rmarkdown::render("cv.Rmd",
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
                        output = "docs/cv.pdf")
+system('evince docs/cv.pdf', wait=FALSE)
