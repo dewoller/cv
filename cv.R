7d3b59a260bbe703118175eecb6c9a47285274d4
Dennis Wollersheim <dewoller@gmail.com>
2021-06-14 07:43:44 +1000

    publish

--- cv.R
+++ cv.R
@@ -1,15 +1,15 @@
-datadrivencv::use_datadriven_cv(
-  full_name = "Dennis Wollersheim",
-  data_location = "https://docs.google.com/spreadsheets/d/1HPZjdhRq8xLfsWQI2AJXZ_ytuWiEktI93Rg_qz_85a4/edit?usp=sharing",
-  pdf_location = "https://github.com/dewoller/cv/raw/master/docs/cv.pdf",
-  html_location = "https://dewoller.github.io/cv/cv.html",
-  source_location = "https://github.com/dewoller/cv",
-  open_files=FALSE
-)
-
-devtools::install_github("nstrayer/datadrivencv")
-
-params = list(pdf_mode = FALSE)
+# datadrivencv::use_datadriven_cv(
+#   full_name = "Dennis Wollersheim",
+#   data_location = "https://docs.google.com/spreadsheets/d/1HPZjdhRq8xLfsWQI2AJXZ_ytuWiEktI93Rg_qz_85a4/edit?usp=sharing",
+#   pdf_location = "https://github.com/dewoller/cv/raw/master/docs/cv.pdf",
+#   html_location = "https://dewoller.github.io/cv/cv.html",
+#   source_location = "https://github.com/dewoller/cv",
+#   open_files=FALSE
+# )
+#
+# devtools::install_github("nstrayer/datadrivencv")
+#
+# params = list(pdf_mode = FALSE)
 
 
 # Knit the HTML version
@@ -26,4 +26,6 @@ rmarkdown::render("cv.Rmd",
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
                        output = "docs/cv.pdf")
-system('evince docs/cv.pdf', wait=FALSE)
+# system('evince docs/cv.pdf', wait=FALSE)
+
+
